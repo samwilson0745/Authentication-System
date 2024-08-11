d9a7a5ac93e6a25d91da5a20621235b7d09ff8ab
samwilson0745 <sohamdeb0745@gmail.com>
2024-08-11 15:20:51 +0530

    chores: server setup done

---
+++ server.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	ID       string `json:"id"`
+	Username string `json:"username"`
+	Email    string `json:"email"`
+}
+
+var users []User
+
+func main() {
+	connect()
+	router := mux.NewRouter()
+	router.HandleFunc("/users", getUsers).Methods("GET")
+	router.HandleFunc("/users", createUser).Methods("POST")
+
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func getUsers(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(users)
+}
+
+func createUser(w http.ResponseWriter, r *http.Request) {
+	var newUser User
+	err := json.NewDecoder(r.Body).Decode(&newUser)
+	if err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+	users = append(users, newUser)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newUser)
+}
