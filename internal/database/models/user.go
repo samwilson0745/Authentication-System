7e1b82f1f1ee9c7448c0e1e39ec67997a2af16f7
samwilson0745 <sohamdeb0745@gmail.com>
2024-08-14 01:26:07 +0530

    refactor: model refactored

--- internal/database/models/user.go
+++ internal/database/models/user.go
@@ -1,12 +1,17 @@
 package models
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type User struct {
 	gorm.Model
-	Name     string `json:"name"`
-	Email    string `json:"email" gorm:"unique"`
-	Password string `json:"password"`
+	ID        uint   `gorm:"primaryKey"`
+	Name      string `json:"name" validate:"required,min=3,max=50"`
+	Email     string `json:"email" gorm:"unique" validate:"required,email"`
+	Password  string `json:"password" validate:"required,min=8"`
+	CreatedAt time.Time
+	UpdatedAt time.Time
 }
