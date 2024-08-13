2493a542cc5503b195723a568a87d1110660be8c
samwilson0745 <sohamdeb0745@gmail.com>
2024-08-13 21:05:53 +0530

    refactor: added password field

--- internal/database/models/user.go
+++ internal/database/models/user.go
@@ -6,6 +6,7 @@ import (
 
 type User struct {
 	gorm.Model
-	Name  string `json:"name"`
-	Email string `json:"email" gorm:"unique"`
+	Name     string `json:"name"`
+	Email    string `json:"email" gorm:"unique"`
+	Password string `json:"password"`
 }
