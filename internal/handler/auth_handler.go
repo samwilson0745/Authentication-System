42bbdc3b46e19e443576263e4ea5859751d12ac3
samwilson0745 <sohamdeb0745@gmail.com>
2024-08-14 01:27:04 +0530

    feat: auth handler with proper validation added

---
+++ internal/handler/auth_handler.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"authsys/internal/database/models"
+	"authsys/internal/service"
+	"authsys/internal/utils"
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type AuthHandler struct {
+	Service *service.AuthService
+}
+
+func (h *AuthHandler) SignIn(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var user models.User
+	var message string
+	validate := validator.New()
+	if err := json.NewDecoder(r.Body).Decode(&user); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	err := validate.Struct(user)
+	if err != nil {
+		validationErrors := make(map[string]string)
+		for _, err := range err.(validator.ValidationErrors) {
+			field := err.Field()
+			tag := err.Tag()
+			validationErrors[field] = fmt.Sprintf("Field '%s' failed validation for tag '%s'", field, tag)
+		}
+		w.WriteHeader(http.StatusBadRequest)
+
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"message": "Validation error",
+			"details": validationErrors,
+		})
+		return
+	}
+	// Hash the password before saving
+	hashedPassword, err := utils.HashPassword(user.Password)
+	if err != nil {
+		http.Error(w, "Error hashing password", http.StatusInternalServerError)
+		return
+	}
+	user.Password = hashedPassword
+	message, err = h.Service.SignIn(&user)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
+
+	json.NewEncoder(w).Encode(map[string]interface{}{
+		"message": message,
+	})
+}
