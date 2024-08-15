12b1dcb579b299bd401d6038a37559e9e8a7e024
samwilson0745 <sohamdeb0745@gmail.com>
2024-08-15 23:50:03 +0530

    refactor: added login service

--- internal/service/auth_service.go
+++ internal/service/auth_service.go
@@ -2,7 +2,9 @@ package service
 
 import (
 	"authsys/internal/database/models"
+	"errors"
 
+	"golang.org/x/crypto/bcrypt"
 	"gorm.io/gorm"
 )
 
@@ -16,4 +18,23 @@ func (s *AuthService) SignIn(user *models.User) (string, error) {
 		return "User registered", nil
 	}
 	return "Error while register user", nil
+}
+func (s *AuthService) Login(email string, password string) (string, error) {
+	var user models.User
+	// Find the user by email
+	result := s.DB.Where("email = ?", email).First(&user)
+	if result.Error != nil {
+		if result.Error == gorm.ErrRecordNotFound {
+			return "User not found", errors.New("user not found")
+		}
+		return "Error querying user", result.Error
+	}
+
+	// Compare the provided password with the hashed password stored in the database
+	err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password))
+	if err != nil {
+		return "Invalid credentials", errors.New("invalid credentials")
+	}
+
+	return "Login successful", nil
 }
